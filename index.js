393159483c8b9cd7e545a35769d5ca93b19c8104
mercy-51769 <51769.hexaware@gmail.com>
2020-04-08 12:07:55 +0530

    package

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const request = require('request');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
