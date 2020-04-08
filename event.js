2ab2c617abe2c7cd88c236fafb15dd2586736d23
mercy-51769 <51769.hexaware@gmail.com>
2020-04-08 11:41:59 +0530

    package

---
+++ event.js
@@ -0,0 +1,132 @@
+{
+  "chennai":[
+    {
+      "event" : 1,
+      "name" :"Little Engineer Summer Camp 2020-Chennai",
+      "synopsis" :"Robotics & Coding,  3D Designing & 3D Printing/Doodling, Electronics & IoT, Machines & Mechanics",
+      "venue": "Crystal Lawn. No 20, 1st street, Haddows Road, Chennai – 600006, Chennai, India",
+       "price":4000,
+       "image":"https://ts-production.imgix.net/images/101026b0-fd9d-44ca-baa7-df78aafcb6a1.jpg?auto=compress,format&w=800&h=450"
+    },
+    {
+      "event" : 1,
+      "name" :"The Kindness Voucher",
+      "synopsis" :" a simple and heartwarming way to bring joy to as many children as possible. These vouchers can be purchased by anyone, to be gifted to children in need. ",
+      "venue": "Chennai, India (Exact venue to be decided)",
+       "price":300,
+       "image":"https://ts-production.imgix.net/images/60749334-5258-4008-ae7c-1f09f3b74804.jpg?auto=compress,format&w=800&h=450"
+    },
+     {
+       "event" : 0,
+      "name" :"SBW : ERACE CANCER RUN 2020",
+      "synopsis" :"Let US ERACE CANCER Run is an initiative by Sankalp Beautiful World, a non profitable organisation working towards the awareness of cancer cure.",
+      "venue": "Anna University, Chennai, India",
+       "price":600,
+       "image":"https://media.newyorker.com/photos/5d9f7a50be0cf50008782e65/master/w_2560,h_1707,c_limit/Caesar-TwoHourMarathonAttempt.jpg"
+    },
+    {
+      "event" : 0,
+      "name" :"IoT Product Development Camp",
+      "synopsis" :"As a 15-hour training offered by our organization, IoT is one of the most advanced and emerging technologies, bringing a close connection with all devices around us including aplliances like FAn, Light and all electronic devices, sometimes giving it to the chairs and tables too. Obvious Fun indeed.",
+      "venue":"341, 2nd Avenue, G Block, Anna Nagar West, Anna Nagar, Chennai, India",
+       "price":2500,
+       "image":"https://ts-production.imgix.net/images/c4a49914-0951-426f-bfc9-cb531a7a5478.jpg?auto=compress,format&w=800&h=450"
+    
+    },
+    {
+     "event" : 0,
+      "name" :"IoT Product Development Camp",
+      "synopsis" :"As a 15-hour training offered by our organization, IoT is one of the most advanced and emerging technologies, bringing a close connection with all devices around us including aplliances like FAn, Light and all electronic devices, sometimes giving it to the chairs and tables too. Obvious Fun indeed.",
+      "venue":"341, 2nd Avenue, G Block, Anna Nagar West, Anna Nagar, Chennai, India",
+       "price":2500,
+       "image":"https://ts-production.imgix.net/images/c4a49914-0951-426f-bfc9-cb531a7a5478.jpg?auto=compress,format&w=800&h=450"
+    }
+  ],
+  
+  "bangalore":[
+     {
+       "event" : 0,
+       "name" :"Kangen Business Mastery",
+       "synopsis":"Strong, Capable and Consistent Team",
+       "venue":"Bengaluru",
+       "price":200,
+       "image":"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F90265189%2F279673266134%2F1%2Foriginal.20191229-012023?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C39%2C1348%2C674&s=93880aa170f09095650fecb2a95ea373"
+    },
+     {
+       "event" : 0,
+       "name" :"Stage Adda presents",
+       "synopsis":"After the grand success of his solo show on Amazon Prime Video, The MahaCool boy is back with his new comedy special. He has a unique style of storytelling and his humor is universal, connecting with all segments of people breaking social and age group barriers.",
+       "venue":"Mother Tekhla Auditorium: Bengaluru",
+       "price":499,
+       "image":"https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-stage-adda-presents-nishant-tanwar-live-2020-2-10-t-15-37-40.jpg"
+    }, 
+    {
+       "event" : 2,
+       "name" :"Bas Kar Bassi Feat. Anubhav Singh Bassi",
+       "venue":"MLR Convention Centre: Whitefield, Bengaluru",
+       "price":499,
+       "synopsis":"Anubhav Singh Bassi",
+       "image":"https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-bas-kar-bassi-feat-anubhav-singh-bassi-2019-8-6-t-19-18-56.jpg"
+    },
+    {
+       "event" : 2,
+       "name" :"Friday Night Comedy",
+       "venue":"Cuckoo Hostel: Bengaluru",
+       "price":99,
+       "synopsis":"Hippo Laugh Club presents a Friday evening StandUp Comedy Open mic loaded with lots of stories and jokes with 7 Comedians.",
+       "image":"https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-friday-night-comedy-2020-3-5-t-16-54-50.jpg"
+      
+    },
+    {
+       "event" : 1,
+       "name" :"Kids! Learn Photography",
+       "venue":"The Studio by LVDS: Bengaluru",
+       "price":4950,
+       "image":"https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-kids-learn-photography-2020-3-17-t-15-52-9.jpg",
+       "synopsis":"Learn Photography"
+    }
+  ],
+  "hyderabad":[
+    {
+      "event" : 2,
+       "name" :"Melodious Moments with Sunitha",
+       "venue":"Shilpakala Vedika: Hyderabad",
+       "price":499,
+       "image":"https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-melodious-moments-with-sunitha-2020-2-26-t-17-5-14.jpg",
+       "synopsis":"We take you to a melodious journey with the queen of melody, Sunitha Upadrashta. This isn`t just a musical concert, it will be an experience altogether."
+    },
+    {
+       "event" : 1,
+       "name" :"Metamorphosis Residential Summer Camp",
+       "venue":"Hyderabad",
+       "price":20882,
+       "image":"https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-metamorphosis-residential-summer-camp-2020-3-9-t-11-4-47.jpg",
+       "synopsis":"Education, Self Improvement, Yoga"
+    },
+    {
+      "event" : 2,
+       "name" :"Acting Workshop by Tollywood Film Director",
+       "venue":"Star Cast - Acting Institute Ameerpet: Hyderabad",
+       "price":25000,
+       "image":"https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-metamorphosis-residential-summer-camp-2020-3-9-t-11-4-47.jpg",
+       "synopsis":"Acting"
+    },
+    {
+      "event" : 2,
+       "name" :"Ultimate Quarantine Week ft Shibani Kashyap",
+       "venue":"Online Streaming",
+       "price":20882,
+       "image":"https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-ultimate-quarantine-week-ft-shibani-kashyap-2020-4-3-t-19-41-56.jpg",
+       "synopsis":"Shibani Kashyap is a multi-talented popular playback and pop singer, composer and live performer whose voice received unique recognition starting with the signature tune of Air FM channel of All India Radio."
+    },
+   {
+      "event" : 2,
+       "name" :"Lauv",
+       "venue":"Online Streaming",
+       "price":2100,
+       "image":"https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-sunburn-arena-with-lauv-2019-11-15-t-21-26-16.jpg",
+       "synopsis":"After climbing the global music charts to the very top, American singer, songwriter, multi-instrumentalist and producer LAUV is back with his latest album and tour ‘How I’m Feeling’ in India this June 2020!"
+      
+    }
+    ]
+}
---
+++ movie.js
@@ -0,0 +1,189 @@
+{
+  "chennai":[
+    {
+      "name" :"Gypsy",
+      "rating" : 3.0,
+      "synopsis" :"A wandering musician and a girl from an orthodox Muslim household end up getting married, but the survival of their relationship becomes a big question following a communal riot.",
+      "actor": "Jiiva",
+      "director": "Raju Murugan",
+      "language":	"Tamil",
+      "theatre": "Sathyam Cinemas, Royapettah, Chennai",
+       "adultprice":160,
+       "childprice":120,
+	   "image":"https://indiaglitz-media.s3.amazonaws.com/tamil/home/gypsy-movie-review-05032020m.jpg"
+    },
+    {
+      "name" :"Asuraguru",
+      "rating" : 2.4,
+      "synopsis" :"Shakti, who becomes obsessed with money from childhood, goes on to become a tech-savvy thief. However, he becomes a changed man due to certain circumstances when cops nab him",
+      "actor": "Vikram prabhu",
+      "director": "Rajdeep",
+      "language":	"Tamil",
+       "theatre": "Palazzo Cinemas, Vadapalani, Chennai",
+       "adultprice":160,
+       "childprice":120,
+	   "image":"https://static.toiimg.com/photo/msid-74611011/74611011.jpg?68488"
+    },
+     {
+      "name" :"Darbar",
+      "rating" : 3.0,
+      "synopsis" :"A top cop is tasked with ending the drug menace in Mumbai, and to do this, he has to take on a most feared and ruthless mafia lord.",
+      "actor": "Rajinikanth",
+      "director": "AR Murugadoss",
+      "language":	"Tamil",
+       "theatre": "S2 Theyagaraja, Thiruvanmiyur, Chennai",
+       "adultprice":160,
+       "childprice":120,
+	   "image":"https://m.media-amazon.com/images/M/MV5BMTVmODRhY2EtZmYyZi00Njk5LWIzNzUtZTllYzU4YzlkNTNhXkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_.jpg"
+    },
+    {
+      "name" :"Oh my kadavule",
+      "rating" : 3.4,
+      "synopsis" :"A young man marries his friend without having any romantic feelings for her, only to end up in a family court asking for divorce. What if he gets a second chance to work things out?",
+      "actor": "Ashok Selvan",
+      "director": "Ashwath Marimuthu",
+      "language":	"Tamil",
+       "theatre": "Kamala Cinemas, Vadapalani, Chennai",
+       "adultprice":160,
+       "childprice":120,
+	   "image":"https://static.toiimg.com/thumb/msid-71633130,imgsize-715735,width-800,height-600,resizemode-75/71633130.jpg"
+    },
+    {
+      "name" :"Pattas",
+      "rating" : 2.5,
+      "synopsis" :"A petty thief comes to know of his illustrious father and takes on the man who murdered him to bring to limelight the ancient martial art form that his father practised.",
+       "actor": "Dhanush",
+      "director": "R.S.Durai Senthilkumar",
+       "language":	"Tamil",
+       "theatre": "Escape Cinemas, Royapettah, Chennai",
+       "adultprice":160,
+       "childprice":120,
+	   "image":"https://i.ytimg.com/vi/z3zCeZJh0h4/maxresdefault.jpg"
+    }
+  ],
+  
+  "bangalore":[
+     {
+      "name" :"Tanhaji",
+      "rating" : 3.5,
+      "synopsis" :"Tanhaji, a Maratha warrior, is Shivaji Maharaj's trusted lieutenant. When the Mughals invade and conquer Kondhana fort, he sets out to reclaim it for his king and country.",
+      "actor": "Ajay Devgn",
+      "director": "Om Raut",
+      "language":	"Hindi",
+      "theatre": "Sri Radhakrishna Theatre, RT Nagar, Bengaluru",
+       "adultprice":160,
+       "childprice":120,
+	   "image": "https://i.ytimg.com/vi/cffAGIYTEHU/maxresdefault.jpg"
+    },
+     {
+      "name" :"Baaghi 3",
+      "rating" : 2.8,
+      "synopsis" :" Fiercely protective of his elder brother Vikram (Riteish Deshmukh), Ronnie (Tiger Shroff) goes after a dreaded terrorist group in Syria to save him. Can he bring back his brother alive?",
+      "actor": "Tiger Shroff",
+      "director": "Ahmed Khan",
+       "language":	"Hindi",
+      "theatre": "PVR Koramangla, Adugodi, Bengaluru",
+       "adultprice":160,
+       "childprice":120,
+	   "image": "https://bloximages.chicago2.vip.townnews.com/indiawest.com/content/tncms/assets/v3/editorial/1/21/121a3814-67ac-11ea-8dbe-0fbb953b63cb/5e6fb887063ff.image.jpg?resize=480%2C630"
+    }, 
+    {
+      "name" :"Shivaarjun",
+      "rating" : 2.6,
+      "synopsis" :"Ramadurga and Rayadurga are neighbouring villages that have been involved in an age-old feud. When the chief of one village seeks revenge, the dispute turns into a blood bath. Can the chief of the other village save his village and his family?",
+      "actor": "Chiranjeevi Sarja",
+      "director": "Shiva Tejass",
+      "language":	"Kannada",
+      "theatre": "Sampige Theatre,Malleshwaram, Bengaluru",
+       "adultprice":160,
+       "childprice":120,
+	   "image": "https://static.toiimg.com/photo/msid-74592060/74592060.jpg?45808"
+    }, 
+    {
+      "name" :"Malang",
+      "rating" : 3.0,
+      "synopsis" :"A wandering musician and a girl from an orthodox Muslim household end up getting married, but the survival of their relationship becomes a big question following a communal riot.",
+       "actor": "Aditya Roy Kapur",
+      "director": "Mohit suri",
+      "language":	"Hindi",
+      "theatre": "Pushpanjali Theatre,Dooravani Nagar, Bengaluru",
+       "adultprice":160,
+       "childprice":120,
+	   "image": "https://images.newindianexpress.com/uploads/user/imagelibrary/2020/1/31/w1200X800/Malang.jpg"
+    }, 
+    {
+      "name" :"Nam Kathe Nim Jothe",
+      "rating" : 2.6,
+      "synopsis" :"Three close friends Deekshith, Mohan and Harish set out to pursue their dreams after completing their engineering. While Deekshith is keen on working on an innovation, Mohan preps himself to write a competitive exam, and Harish just hopes to relax and enjoy his life.",
+      "actor": "Deepak BC",
+      "director": "Deepak BC",
+      "language":	"kannada",
+      "theatre": "Ganesh Theatre,Bangalore Urban, Bengaluru",
+       "adultprice":160,
+       "childprice":120,
+	   "image": "https://filmgappa.com/wp-content/uploads/2019/03/IMG-20190305-WA0004-682x1024.jpg"
+    }
+  ],
+  "hyderabad":[
+    {
+      "name" :"Angrezi Medium",
+      "rating" : 3.5,
+      "synopsis" :" An emotional story packed with powerful performances",
+      "actor": "Irrfan Khan",
+      "director": "Homi Adajania",
+      "language":	"Hindi",
+      "theatre": "PVR Cinemas, Hyderabad",
+       "adultprice":160,
+       "childprice":120,
+	   "image": "https://cdn.siasat.com/wp-content/uploads/2020/02/Angrezi-Medium-1.jpg"
+    },
+    {
+      "name" :"Eureka",
+      "rating" : 3.4,
+      "synopsis" : " Yuva (Karteek Anand) hates his college to the core, so much so that he wants to put an end to the college fest being planned by Revanth (Munna). When the latter dies a mysterious death during the said fest, what unfolds is chaos." ,
+      "actor": "Karteek Anand",
+      "director": "Karteek Anand",
+      "language":	"Telugu",
+      "theatre": "INOX Maheshwari Parameshwari, Hyderabad",
+       "adultprice":160,
+       "childprice":120,
+	   "image": "https://i0.wp.com/www.socialnews.xyz/wp-content/uploads/2019/04/06/eureka-Movie-ugadi-wishes-poster-.jpg?fit=2475%2C1392&quality=90&zoom=1&ssl=1"
+    },
+    {
+      "name" :"Madha",
+      "rating" : 3.3,
+      "synopsis" :"A proof-reader named Nisha (Trishna Mukherjee) begins dating a cinematographer called Arjun (Venkat Rahul), unknowing of his devious plans for her.",
+      "actor": "Trishna Mukherjee",
+      "director": "Srividya Basawa",
+      "language":	"Telugu",
+      "theatre": "BVK Multiplex Vijayalakshmi Cinemas, Hyderabad",
+       "adultprice":160,
+       "childprice":120,
+	   "image": "https://m.media-amazon.com/images/M/MV5BYjk0NWY0MDgtNmI2OC00OGEwLTllYjctNmU0YzRhNDM5Mjg3XkEyXkFqcGdeQXVyNzgxMjUzNzk@._V1_.jpg"
+    },
+    {
+      "name" :"Prema Pipasi ",
+      "rating" : 1.8,
+       "synopsis" :"Prema Pipasi is the story of a guy (GPS) who cheats on and sleeps with multiple women to find his ‘one true love’. What happens when he meets his old girlfriend (Kapilakshi Malhotra)?",
+      "actor": "GPS",
+      "director": "Murali Ramaswamy",
+      "language":	"Telugu",
+      "theatre": "Asian Shiva Ganga, Hyderabad",
+       "adultprice":160,
+       "childprice":120,
+	   "image": "https://1.bp.blogspot.com/-IsBlH3v8jp8/XmyDtHNk3aI/AAAAAAAAGls/JGP_dPeiSgETmpVNUFzebP_tXmq8DDdaACLcBGAsYHQ/s1600/premapipasi.jpg"
+    },
+   {
+      "name" :"Love Aaj Kal",
+      "rating" : 3.5,
+      "synopsis" :"Twenty-two-year-old Zoe Chauhan (Sara Ali Khan) is averse to a serious long term relationship. She wants to have fun with no strings attached and pursue her career. But another love story from the past makes her question her own choices",
+      "actor": "Kartik Aaryan",
+      "director": "Imtiaz Ali",
+      "language":	"Hindi",
+      "theatre": "Prasads Multiplex, Hyderabad",
+       "adultprice":160,
+       "childprice":120,
+	   "image": "https://upload.wikimedia.org/wikipedia/en/4/4c/Love_Aaj_Kal_film_poster.jpg"
+    }
+    ]
+}
