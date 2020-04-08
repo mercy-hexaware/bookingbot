a6c058e104e315c0bb03a4fe04e30562fe8f106e
mercy-51769 <51769.hexaware@gmail.com>
2020-04-08 12:14:07 +0530

    package

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
+app.post('/booking', (req, res) => {
+	console.log('webhook');
+    console.log(req.body);
+});
