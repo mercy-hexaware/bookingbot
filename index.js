724437a42fdc5c606f62738ec8d0b2d1c4096789
mercy-51769 <51769.hexaware@gmail.com>
2020-04-12 15:21:29 +0530

    respone

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
-let location, booking_movie, ticket_count, booking_date, booking_time, event_name, event_count, event_date, event_no;
+let location, booking_movie, ticket_count, booking_date, booking_time, event_name, event_count, event_date, event_no, confirm;
 app.post('/booking', (req, res) => {
 	console.log('webhook');
     console.log(req.body);	
@@ -61,7 +61,8 @@ app.post('/booking', (req, res) => {
         console.log('booking_date',req.body.queryResult.parameters['booking_date']);
 		booking_date = req.body.queryResult.parameters['booking_date'];	
 	}else if(req.body.queryResult.intent.displayName === 'booking-movie-ticket-time'){
-		let i, indexNo, moviedetails, subtotal, tax
+		let i, indexNo, moviedetails, subtotal, tax;
+		confirm = "movie";
         booking_time = req.body.originalDetectIntentRequest.payload.data.message.quick_reply['payload'].toLowerCase();
 	    console.log('booking_time',booking_time);
 		for (i in movies[location]) {
@@ -112,90 +113,7 @@ app.post('/booking', (req, res) => {
 				}
 			}		  
 		});
-	}
-	else if(req.body.queryResult.intent.displayName === 'Payment_card-number-mobno-otp'){
-		let customDel, j,customerData,i, ticket_count, booking_date, booking_time, payment_card, card_number,phone_number,given_name,moviedetails, subtotal, total_cost,indexNo, indexJ ;
-		console.log('outputContexts',req.body.queryResult.outputContexts);
-		customDel = req.body.queryResult.outputContexts;
-		for (j in customDel) {
-			if(customDel[j].lifespanCount){
-				indexJ = j;
-				console.log('j',indexJ);
-			}
-		}
-		console.log('customerData',req.body.queryResult.outputContexts[indexJ]);
-		customerData = req.body.queryResult.outputContexts[indexJ];
-		console.log("location",location);		
-		ticket_count = customerData.parameters["ticket_count"];
-		booking_date = customerData.parameters["booking_date"];	  	
-		booking_time = customerData.parameters["booking_time.original"];
-		payment_card = customerData.parameters["payment_card"];
-		card_number = customerData.parameters["card-number"];
-		phone_number = customerData.parameters["phone-number"];
-		given_name = customerData.parameters["given-name"];
-		console.log('booking_movie',booking_movie);
-		for (i in movies[location]) {
-			if(movies[location][i].name.toLowerCase() == booking_movie){
-				indexNo = i;
-				console.log('i',indexNo);				
-			}
-		}
-		console.log('moviedetails',movies[location][indexNo]);
-		moviedetails = movies[location][indexNo];		
-		subtotal = ticket_count * eval(moviedetails["price"]);
-		let tax = eval(subtotal) * eval(0.06);		
-		let taxvalue = fmtPrice(tax);
-		console.log('taxvalue',taxvalue);
-		total_cost = eval(subtotal) + eval(taxvalue);
-		console.log('total_cost',total_cost);
-		total_cost = fmtPrice(total_cost);		
-		let date = new Date(booking_date);
-		let year = date.getFullYear();
-		let month = date.getMonth()+1;
-		let dt = date.getDate();
-		if (dt < 10) {
-		  dt = '0' + dt;
-		}
-		if (month < 10) {
-		  month = '0' + month;
-		}
-		let bookDay = year+'-' + month + '-'+dt;
-		console.log(year+'-' + month + '-'+dt);
-		return res.json({
-			"fulfillmentText": "Movie Ticket",			
-			"source": "facebook",
-			'payload': {
-				"facebook": {
-					"attachment": {
-					   "type": "template",
-					   "payload": {
-							"template_type": "receipt",
-							"recipient_name": given_name, 
-							"order_number": "12345678902",
-							"currency": "INR",
-							"payment_method": payment_card +" "+ card_number,        
-							"timestamp": Math.floor(Date.now() / 1000),        
-							"summary": {
-							  "subtotal": subtotal,                              						  
-							  "total_tax": taxvalue,
-							  "total_cost": total_cost
-							},       
-							"elements": [
-								{
-									"title": booking_movie[0].toUpperCase() + booking_movie.slice(1),
-									"subtitle": bookDay +" "+ booking_time +", "+  moviedetails["theatre"],
-									"quantity": ticket_count,
-									"price": moviedetails["price"],
-									"currency": "INR",
-									"image_url": moviedetails["image"]
-								}
-							]
-						}
-					}
-				}
-			}		  
-		});
-	}
+	}	
 	else if(req.body.queryResult.intent.displayName === 'events - city'){
 		console.log('data', req.body.originalDetectIntentRequest.payload.data.message['text'].toLowerCase());
 		let  x=[], i, info;
@@ -281,8 +199,9 @@ app.post('/booking', (req, res) => {
 	}
 	else if(req.body.queryResult.intent.displayName === 'events - booking-ticket -count'){
 		let i, eventdetails, subtotal, tax;
-        console.log('event_count',req.body.queryResult.parameters['ticketCount']);
-		event_count = req.body.queryResult.parameters['ticketCount'];	    
+		confirm = "event";
+        console.log('event_count',req.body.queryResult.parameters['ticket_count']);
+		event_count = req.body.queryResult.parameters['ticket_count'];	    
 		console.log('event_no',event_no);
 		console.log('eventdetails',events[location][event_no]);
 		eventdetails = events[location][event_no];		
@@ -327,7 +246,105 @@ app.post('/booking', (req, res) => {
 			}		  
 		});
 	}
-	
+	else if(req.body.queryResult.intent.displayName === 'Payment_card-number-mobno-otp'){
+		let customDel, j,customerData,i, ticket_count, booking_date, booking_time, payment_card, card_number,phone_number,given_name,moviedetails, subtotal, total_cost,indexNo, indexJ ;
+		console.log('outputContexts',req.body.queryResult.outputContexts);
+		customDel = req.body.queryResult.outputContexts;
+		for (j in customDel) {
+			if(customDel[j].lifespanCount){
+				indexJ = j;
+				console.log('j',indexJ);
+			}
+		}
+		console.log('customerData',req.body.queryResult.outputContexts[indexJ]);
+		customerData = req.body.queryResult.outputContexts[indexJ];
+		console.log("location",location);		
+		ticket_count = customerData.parameters["ticket_count"];
+		payment_card = customerData.parameters["payment_card"];
+		card_number = customerData.parameters["card-number"];
+		phone_number = customerData.parameters["phone-number"];
+		given_name = customerData.parameters["given-name"];
+		if(confirm === "movie"){
+			booking_date = customerData.parameters["booking_date"];	  	
+			booking_time = customerData.parameters["booking_time.original"];
+			console.log('booking_movie',booking_movie);
+			for (i in movies[location]) {
+				if(movies[location][i].name.toLowerCase() == booking_movie){
+					indexNo = i;
+					console.log('i',indexNo);				
+				}
+			}
+			console.log('moviedetails',movies[location][indexNo]);
+			moviedetails = movies[location][indexNo];		
+			subtotal = ticket_count * eval(moviedetails["price"]);
+		}
+		else{
+			booking_date = event_date;
+			console.log('event_name',event_name);
+			for (i in events[location]) {
+				if(events[location][i].name.toLowerCase() === event_name){
+					indexNo = i;
+					console.log('i',indexNo);				
+				}
+			}
+			console.log('eventdetails',events[location][indexNo]);
+			moviedetails = events[location][indexNo];		
+			subtotal = ticket_count * eval(moviedetails["price"]);
+		}		
+		let tax = eval(subtotal) * eval(0.06);		
+		let taxvalue = fmtPrice(tax);
+		console.log('taxvalue',taxvalue);
+		total_cost = eval(subtotal) + eval(taxvalue);
+		console.log('total_cost',total_cost);
+		total_cost = fmtPrice(total_cost);		
+		let date = new Date(booking_date);
+		let year = date.getFullYear();
+		let month = date.getMonth()+1;
+		let dt = date.getDate();
+		if (dt < 10) {
+		  dt = '0' + dt;
+		}
+		if (month < 10) {
+		  month = '0' + month;
+		}
+		let bookDay = year+'-' + month + '-'+dt;
+		console.log(year+'-' + month + '-'+dt);
+		confirm = "";
+		return res.json({
+			"fulfillmentText": "Movie Ticket",			
+			"source": "facebook",
+			'payload': {
+				"facebook": {
+					"attachment": {
+					   "type": "template",
+					   "payload": {
+							"template_type": "receipt",
+							"recipient_name": given_name, 
+							"order_number": "12345678902",
+							"currency": "INR",
+							"payment_method": payment_card +" "+ card_number,        
+							"timestamp": Math.floor(Date.now() / 1000),        
+							"summary": {
+							  "subtotal": subtotal,                              						  
+							  "total_tax": taxvalue,
+							  "total_cost": total_cost
+							},       
+							"elements": [
+								{
+									"title": booking_movie[0].toUpperCase() + booking_movie.slice(1),
+									"subtitle": bookDay +" "+ booking_time +", "+  moviedetails["theatre"]? moviedetails["theatre"]:moviedetails["venue"],
+									"quantity": ticket_count,
+									"price": moviedetails["price"],
+									"currency": "INR",
+									"image_url": moviedetails["image"]
+								}
+							]
+						}
+					}
+				}
+			}		  
+		});
+	}
 function fmtPrice(tax)
  	{
 		let result=Math.floor(tax)+".";
